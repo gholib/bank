2d4bf7d1526e04031fa72f5549342b36db0475d5
gholib <gtrandnet@gmail.com>
2020-09-26 09:56:46 +0500

    init types module

--- pkg/bank/types/types.go
+++ pkg/bank/types/types.go
@@ -12,3 +12,34 @@ type Payment struct {
 	Amount   Money
 	Category Category
 }
+
+//Currency currency code
+type Currency string
+
+//currency codes
+const (
+	TJS Currency = "TJS"
+	RUB Currency = "RUB"
+	USD Currency = "USD"
+)
+
+//PAN card number
+type PAN string
+
+//Card payments card
+type Card struct {
+	ID         int
+	PAN        PAN
+	Balance    Money
+	MinBalance Money
+	Currency   Currency
+	Color      string
+	Name       string
+	Active     bool
+}
+
+type PaymentSource struct {
+	Type    string
+	Number  string
+	Balance Money
+}
